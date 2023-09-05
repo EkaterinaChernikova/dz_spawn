1e895233519b4ebf7d7eb25f32b38b7aa0ce44d6
Chernikova <104696269+EkaterinaChernikova@users.noreply.github.com>
2023-09-05 14:52:55 +0300

    Доработка

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Movement : MonoBehaviour
+{
+    private const string Walk = nameof(Walk);
+
+    private Animator _animator;
+    private float _movementSpeed = 2.0f;
+    public readonly int _walk = Animator.StringToHash(Walk);
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetBool(_walk, true);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _movementSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     [SerializeField] private float _delay = 2.0f;
-    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Movement _enemy;
 
-    private Transform[] _pointsArray;
+    private Transform[] _points;
 
     private Coroutine _enemySpawnCoroutine;
 
     private int _randomPoint;
+    private WaitForSeconds _timer;
 
     private void Start()
     {
-        _pointsArray = GetComponentsInChildren<Transform>();
+        _points = GetComponentsInChildren<Transform>();
+        _timer = new WaitForSeconds(_delay);
         _enemySpawnCoroutine = StartCoroutine(SpawnEnemyes());
     }
 
@@ -23,14 +25,14 @@ public class Spawn : MonoBehaviour
     {
         while (true)
         {
-            _randomPoint = Random.Range(0, _pointsArray.Length);
+            _randomPoint = Random.Range(0, _points.Length);
             Instantiate(_enemy, 
                 new Vector3(
-                    _pointsArray[_randomPoint].position.x,
-                    _pointsArray[_randomPoint].position.y,
-                    _pointsArray[_randomPoint].position.z),
-                _pointsArray[_randomPoint].rotation);
-            yield return new WaitForSeconds(_delay);
+                    _points[_randomPoint].position.x,
+                    _points[_randomPoint].position.y,
+                    _points[_randomPoint].position.z),
+                _points[_randomPoint].rotation);
+            yield return _timer;
         }
     }
 }
